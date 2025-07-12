bc0c7f15db5181031b5563800d6e1526e42b0e58
Denys Tkachenko <sinedtex@gmail.com>
2025-07-12 13:07:02 +0300

    fix getMe func

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -4,13 +4,31 @@ import { Note } from "@/types/note";
 import { nextServer } from "./api";
 
 export const getMe = async (): Promise<User | null> => {
-  const cookieStore = await cookies();
-  const { data } = await nextServer.get("/users/me", {
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-  });
-  return data;
+  try {
+    const cookieStore = await cookies();
+
+    const response = await nextServer.get("/users/me", {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+
+    const data = response.data;
+
+    if (
+      !data ||
+      typeof data.avatar !== "string" ||
+      typeof data.username !== "string" ||
+      typeof data.email !== "string"
+    ) {
+      return null;
+    }
+
+    return data;
+  } catch (err) {
+    console.error("getMe() failed:", err);
+    return null;
+  }
 };
 
 export interface FetchNotesProps {
